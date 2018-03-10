571788b1f5e4598320d07c950a755e9c2851ad79
Marija G <marija_grozdanic@yahoo.com>
2018-03-10 16:52:10 +0100

    Create morse_code.py

---
+++ morse_code.py
@@ -0,0 +1,92 @@
+import RPi.GPIO as GPIO
+import time
+from pygame import mixer
+
+
+CODE = {' ': ' ',
+        "'": '.----.',
+        '(': '-.--.-',
+        ')': '-.--.-',
+        ',': '--..--',
+        '-': '-....-',
+        '.': '.-.-.-',
+        '/': '-..-.',
+        '0': '-----',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        ':': '---...',
+        ';': '-.-.-.',
+        '?': '..--..',
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+        '_': '..--.-'}
+ledPin=18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin,GPIO.OUT)
+
+
+def dot():
+	GPIO.output(ledPin,1)
+	time.sleep(0.2)
+	mixer.init()
+        alert=mixer.Sound('Dot.mp3')
+        alert.play()
+	GPIO.output(ledPin,0)
+	time.sleep(0.2)
+
+def dash():
+	GPIO.output(ledPin,1)
+	time.sleep(0.5)
+	mixer.init()
+        alert=mixer.Sound('Dash.mp3')
+        alert.play()
+	GPIO.output(ledPin,0)
+	time.sleep(0.2)
+
+while True:
+        mixer.init()
+        alert=mixer.Sound('Dash.mp3')
+        alert.play()
+	input = raw_input('What would you like to send? ')
+	for letter in input:
+			for symbol in CODE[letter.upper()]:
+				if symbol == '-':
+					dash()
+				elif symbol == '.':
+					dot()
+				else:
+					time.sleep(0.5)
+			time.sleep(0.5)
+def end():
+	GPIO.output(ledPin,GPIO.LOW)
